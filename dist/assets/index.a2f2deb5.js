93ebf88818da861f2abeefaadcaa1b2d1269c1fe
JYM <1253264340@qq.com>
2021-09-15 10:56:53 +0800

    代码优化

--- dist/assets/index.a2f2deb5.js
+++
@@ -1,1 +0,0 @@
-import{o as a,c as r}from"./vendor.aaeada43.js";const e={},n={class:"container"};e.render=function(e,o,t,s,c,d){return a(),r("div",n,"456")};export default e;
---
+++ dist/assets/index.d3d60c2e.js
@@ -0,0 +1,1 @@
+import{o as r,c as n}from"./vendor.8d5dd332.js";const o={},d={class:"container"};o.render=function(o,e,t,s,a,c){return r(),n("div",d,"456")};export default o;
